2f47a43206cbcc4a09d2c2e0cb8eafbf0efc1eef
ITANéA <frederic.blanc@itanea.fr>
2018-11-28 11:52:06 +0100

    after npm install & npm run watch

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,3 +31,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+
+window._ = require('lodash');
+window.Popper = require('popper.js').default;
+try {
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
+} catch (e) {}
